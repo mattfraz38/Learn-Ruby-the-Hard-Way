8fc6330c7767229999467ae9575b17464518d5e5
Matt Frazier <frazier3388@yahoo.com>
2019-09-03 14:20:24 +0900

    exercise one complete

---
+++ mystuff/ex1.rb
@@ -0,0 +1,11 @@
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing"
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
+puts "This is an exercise drill line."
+# This is also an exercise drill
+# The next line won't print in the terminal
+"This is a test line!"
