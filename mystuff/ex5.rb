13316117a6e6c47477bcaa8508329cc261ead4cd
Matt Frazier <frazier3388@yahoo.com>
2019-09-17 15:42:43 +0900

    exercise six complete

--- mystuff/ex5.rb
+++ mystuff/ex5.rb
@@ -13,7 +13,7 @@ puts "He's #{age} years old."
 puts "He's #{height} inches tall."
 puts "He's #{weight} pounds heavy."
 puts "Actually that's not too heavy."
-puts "He's got #{eyes} and #{hair} hair."
+puts "He's got #{eyes} eyes and #{hair} hair."
 puts "His teeth are usually #{teeth} depending on the coffee."
 
 puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
